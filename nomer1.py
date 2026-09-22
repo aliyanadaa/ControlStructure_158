dc98816bad47946f28ebd59957978038b8dbfbf0
aliyaanadaa <aliyanadaq@gmail.com>
2026-09-22 11:27:09 +0700

    jawaban nomer 1

---
+++ nomer1.py
@@ -0,0 +1,11 @@
+persentase = float(input("Masukkan Persentase:"))
+if persentase >= 90:
+    print("Excellent Performance")
+elif persentase >= 80:
+    print("Very Good Performance")
+elif persentase >= 70:
+    print("Good Performance")
+elif persentase >= 60: 
+    print("Average Performance")
+else:
+    print("yh, smgt ok, jgn nyerah")
